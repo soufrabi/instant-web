77b86c4016f9782a6129d6e309dcb68e2c2d0d9a
Anirban Dey <anirbandey@soufrabi.com>
2024-06-03 19:40:32 +0530

    Create Incognito Chat Mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,14 @@
 
 import React from "react"
 import { IoMdSend } from "react-icons/io";
+import { BsIncognito } from "react-icons/bs";
+import { BsChatLeftText } from "react-icons/bs";
+import { IoSettingsOutline } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { HiDotsVertical } from "react-icons/hi";
+import { nanoid } from "nanoid";
 import { socket } from "@/socket"
+import Markdown from "react-markdown";
 
 type ChatMessageData = {
     text: string,
@@ -45,8 +52,9 @@ function ChatArea({ chatMessageList, sendMessage }: ChatAreaProps) {
     }
 
     const handleSendButtonClick = () => {
-        if (composeMessageTextAreaValue.length > 0) {
-            sendMessage(composeMessageTextAreaValue)
+        const messageText = composeMessageTextAreaValue.trim()
+        if (messageText.length > 0) {
+            sendMessage(messageText)
             setComposeMessageTextAreaValue("")
         }
     }
@@ -77,9 +85,14 @@ function ChatArea({ chatMessageList, sendMessage }: ChatAreaProps) {
             >
                 {
                     chatMessageList.map((chatMessage) => (
-                        <div className={`flex flex-row ${chatMessage.from === myID ? "justify-end" : "justify-start"}`} >
+                        <div
+                            key={nanoid()}
+                            className={`flex flex-row ${chatMessage.from === myID ? "justify-end" : "justify-start"}`}
+                        >
                             <div className="p-3 rounded-2xl bg-teal-200">
-                                {chatMessage.text}
+                                <Markdown>
+                                    {chatMessage.text}
+                                </Markdown>
                             </div>
 
                         </div>
@@ -88,25 +101,27 @@ function ChatArea({ chatMessageList, sendMessage }: ChatAreaProps) {
 
                 <div ref={chatListBottomRef}></div>
             </div>
-            <div className="flex flex-row items-center gap-2 px-4 py-3">
-                <textarea
-                    className="flex-1 bg-orange-50 rounded-2xl p-3 outline-yellow-600 resize-none"
-                    value={composeMessageTextAreaValue}
-                    onChange={handleComposeMessageTextAreaValueChange}
-                    onKeyDown={handleComposeMessageKeyDown}
-                />
-                <button
-
-                    className="h-fit bg-white p-2 rounded-2xl"
-                    onClick={handleSendButtonClick}
-                // disabled={composeMessageTextAreaValue.length === 0}
-                >
-
-                    <IoMdSend
-                        className="w-8 h-8 text-green-600"
+            <div className="p-3 bg-gray-100">
+                <div className="flex flex-row items-center gap-2 px-4 py-1">
+                    <textarea
+                        className="flex-1 bg-white rounded-2xl p-3 outline-gray-300 resize-none"
+                        value={composeMessageTextAreaValue}
+                        onChange={handleComposeMessageTextAreaValueChange}
+                        onKeyDown={handleComposeMessageKeyDown}
                     />
+                    <button
 
-                </button>
+                        className="h-fit bg-white p-2 rounded-2xl"
+                        onClick={handleSendButtonClick}
+                    // disabled={composeMessageTextAreaValue.length === 0}
+                    >
+
+                        <IoMdSend
+                            className="w-8 h-8 text-green-600"
+                        />
+
+                    </button>
+                </div>
             </div>
 
 
@@ -179,8 +194,74 @@ export default function Home() {
     }, [])
 
     return (
-        <main className="h-screen w-screen flex flex-row p-5">
-            <div className="w-[calc(30vw)] h-full bg-yellow-100/50">
+        <main className="h-screen w-screen flex flex-row p-5 bg-slate-300">
+
+            <div className="h-full w-16 bg-gray-100 flex flex-col gap-4 py-4 items-center justify-between">
+                <div className="flex flex-col gap-6 py-2">
+                    <button className="">
+                        <BsIncognito
+                            className="w-6 h-6"
+                        />
+                    </button>
+                    <button className="">
+                        <BsChatLeftText
+                            className="w-6 h-6"
+                        />
+                    </button>
+                </div>
+
+                <div className="flex flex-col gap-6 py-2">
+                    <button className="">
+                        <IoSettingsOutline
+                            className="w-6 h-6"
+                        />
+                    </button>
+                    <button className="">
+                        <CgProfile
+                            className="w-6 h-6"
+                        />
+                    </button>
+                </div>
+            </div>
+            <div className="w-[calc(20vw)] h-full flex flex-col bg-white">
+                <div className="flex flex-row py-4 px-4 justify-between items-center">
+                    <span
+                        className="font-bold text-lg"
+                    >Incognito Chat</span>
+                    <div className="cursor-pointer">
+                        <HiDotsVertical
+                            className="w-5 h-5"
+                        />
+                    </div>
+
+                </div>
+
+                <div className="flex flex-row gap-2 px-4 py-2">
+                    <span
+                    >Room No&nbsp;</span>
+                    <input
+                        type="text"
+                        className="border-gray-300 outline-none border-2 px-1"
+                    />
+                </div>
+                <div className="flex flex-row gap-2 px-4 py-2">
+                    <span
+                    >Password</span>
+                    <input
+                        type="password"
+                        className="border-gray-300 outline-none border-2 px-1"
+                    />
+                </div>
+                <div className="flex flex-row gap-2 px-4 py-2">
+                    <button className="text-gray-800 bg-green-200 p-2 rounded-md">
+                        <span
+                        >Join</span>
+                    </button>
+                    <button className="text-gray-800 bg-green-200 p-2 rounded-md">
+                        <span
+                        >Create</span>
+                    </button>
+                </div>
             </div>
             <ChatArea chatMessageList={chatMessageList} sendMessage={sendMessage} />
             {
@@ -196,114 +277,3 @@ export default function Home() {
     )
 }
 
-// export default function Home() {
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-//         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-//           Get started by editing&nbsp;
-//           <code className="font-mono font-bold">src/app/page.tsx</code>
-//         </p>
-//         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-//           <a
-//             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-//             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             By{" "}
-//             <Image
-//               src="/vercel.svg"
-//               alt="Vercel Logo"
-//               className="dark:invert"
-//               width={100}
-//               height={24}
-//               priority
-//             />
-//           </a>
-//         </div>
-//       </div>
-
-//       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-//         <Image
-//           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-//           src="/next.svg"
-//           alt="Next.js Logo"
-//           width={180}
-//           height={37}
-//           priority
-//         />
-//       </div>
-
-//       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-//         <a
-//           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className="mb-3 text-2xl font-semibold">
-//             Docs{" "}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-//             Find in-depth information about Next.js features and API.
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className="mb-3 text-2xl font-semibold">
-//             Learn{" "}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-//             Learn about Next.js in an interactive course with&nbsp;quizzes!
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className="mb-3 text-2xl font-semibold">
-//             Templates{" "}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-//             Explore starter templates for Next.js.
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className="mb-3 text-2xl font-semibold">
-//             Deploy{" "}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-//             Instantly deploy your Next.js site to a shareable URL with Vercel.
-//           </p>
-//         </a>
-//       </div>
-//     </main>
-//   );
-// }
