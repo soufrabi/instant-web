0d940f8d2826b203cd3ab24a815d8d911a2ec2a6
Anirban Dey <anirbandey@soufrabi.com>
2024-06-04 08:17:39 +0530

    use WEBSITE_URL_DOMAIN environment variable

--- src/socket.ts
+++ src/socket.ts
@@ -2,8 +2,10 @@
 
 import { io } from 'socket.io-client'
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? "https://domain.tld" : 'http://localhost:3500';
+const URL = process.env.NODE_ENV === 'production' ? process.env.WEBSITE_URL_DOMAIN : 'http://localhost:3500';
+const PATH = "/socket.io"
 
 export const socket = io(URL, {
-    autoConnect: false
+    autoConnect: false,
+    path: PATH
 })
