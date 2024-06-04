fa7412039ca9abb15ba5b2ce5bf4bdc8e3db3af4
Anirban Dey <anirbandey@soufrabi.com>
2024-06-04 08:23:34 +0530

    Use null forgiving operator for process.env

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,7 @@
 
 import { io } from 'socket.io-client'
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? process.env.WEBSITE_URL_DOMAIN : 'http://localhost:3500';
+const URL = process.env.NODE_ENV === 'production' ? process.env.WEBSITE_URL_DOMAIN! : 'http://localhost:3500';
 const PATH = "/socket.io"
 
 export const socket = io(URL, {
