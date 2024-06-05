8c96cd6873797dd6979919184234965230f7f987
Anirban Dey <anirbandey@soufrabi.com>
2024-06-05 18:05:57 +0530

    Add configuration for next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+import nextPWA from 'next-pwa';
+
+const withPWA = nextPWA({
+    dest: "public",
+    disable: process.env.NODE_ENV === 'development',
+})
+
 const nextConfig = {
     output: "standalone",
 };
 
-export default nextConfig;
+
+// export default nextConfig;
+export default withPWA(nextConfig)
