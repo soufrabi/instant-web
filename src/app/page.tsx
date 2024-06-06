91005a4c49d8215bb442db9e4e37f5238170336d
Anirban Dey <anirbandey@soufrabi.com>
2024-06-06 23:19:14 +0530

    Allow Mode Switching in LeftSmallSideBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,34 +59,59 @@ function LeftSmallSideBar({ appMode, setAppMode }: LeftSmallSideBarProps) {
     return (
 
         <div className="h-full w-16 bg-gray-100 flex flex-col gap-4 py-4 items-center justify-between">
-            <div className="flex flex-col gap-6 py-2">
+            <div className="flex flex-col gap-2">
                 <button
-                    className=""
+                    className={clsx("px-2 py-2 hover:shadow-customhovereffect rounded-2xl",
+                    )}
+                    onClick={() => setAppMode(AppMode.CHAT)}
+                >
+                    <BsChatLeftText
+                        className="w-6 h-6"
+                    />
+                </button>
+                <button
+                    className={clsx("px-2 py-2 hover:shadow-customhovereffect rounded-2xl",
+                    )}
+                    onClick={() => setAppMode(AppMode.COMMUNITY)}
+                >
+                    <HiOutlineUserGroup
+                        className="w-6 h-6"
+                    />
+                </button>
+                <button
+                    className={clsx("px-2 py-2 hover:shadow-customhovereffect rounded-2xl",
+                    )}
+                    onClick={() => setAppMode(AppMode.CALLS)}
+                >
+                    <IoCallOutline
+                        className="w-6 h-6"
+                    />
+                </button>
+                <button
+                    className={clsx("px-2 py-2 hover:shadow-customhovereffect rounded-2xl",
+                    )}
                     onClick={() => { setAppMode(AppMode.INCOGNITO_CHAT) }}
                 >
                     <BsIncognito
                         className="w-6 h-6"
                     />
                 </button>
-                <button
-                    className=""
-                >
-                    <BsChatLeftText
-                        className="w-6 h-6"
-                    />
-                </button>
             </div>
 
-            <div className="flex flex-col gap-6 py-2">
+            <div className="flex flex-col gap-2 py-2">
                 <button
-                    className=""
+                    className={clsx("px-2 py-2 hover:shadow-customhovereffect rounded-2xl",
+                    )}
                     onClick={() => { setAppMode(AppMode.SETTINGS) }}
                 >
                     <IoSettingsOutline
                         className="w-6 h-6"
                     />
                 </button>
-                <button className="">
+                <button
+                    className={clsx("px-2 py-2 hover:shadow-customhovereffect rounded-2xl",
+                    )}
+                >
                     <CgProfile
                         className="w-6 h-6"
                     />
