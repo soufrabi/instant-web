c5039d137cb2f6899a989cca26d5a2621f593f2a
Anirban Dey <anirbandey@soufrabi.com>
2024-06-20 12:24:53 +0530

    Add tailwindcss extension for box shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,8 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
         boxShadow: {
-        "customhovereffect": "inset 0 0 100rem rgba(0,0,0,0.05), 0 0 100rem rgba(0,0,0,0.5)"
+        "customhovereffect": "inset 0 0 100rem rgba(0,0,0,0.05), 0 0 100rem rgba(0,0,0,0.5)",
+        "customalldirectionmd": "0 0px 6px 0 rgb(0 0 0 / 0.1), 0 0px 6px -1px rgb(0 0 0 / 0.1)"
       }
 
     },
