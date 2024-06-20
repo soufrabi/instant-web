96659499283bf5163132e80a5e8fc195eafa0fae
Anirban Dey <anirbandey@soufrabi.com>
2024-06-20 12:28:25 +0530

    Create next-auth api route and middleware

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
+
+export default withAuth(
+    function middleware(req) {
+        console.log("req.nextUrl.pathname", req.nextUrl.pathname)
+        console.log("req.nextUrl.pathname", req.nextauth.token)
+    }
+
+)
+
+export const config = {
+    matcher: ["/"]
+}
