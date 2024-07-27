61143d5079b5ca433db96679f5ff1063a464576f
Anirban Dey <anirbandey@soufrabi.com>
2024-07-27 19:05:23 +0530

    move next-auth options to lib directory, pass prisma adapter

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth/next";
-import { options } from "./options"
+import { options } from "@/lib/auth/options"
 
 const handler = NextAuth(options)
 export { handler as GET, handler as POST }
